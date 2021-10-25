a479706d54184b5132df97837179d2ef4bb6c185
leonokida <le0n0kida@gmail.com>
2021-10-25 17:05:08 -0300

    [add] - Declaração das variáveis, leitura e processamento das funções

--- ep02.c
+++ ep02.c
@@ -5,9 +5,35 @@
 #include <stdlib.h>
 #include <math.h>
 #include <matheval.h>
+#include "utils.h"
 
 #define MAX 50
 
 int main() {
+    /* Declarando variáveis */
+    int n, k, maxIt;
+    double epsilon;
+    char funcao[MAX];
+
+    /* Lê dimensão da matriz e diagonais */
+    scanf("%d %d", &n, &k);
+
+    /* Declara e lê vetor de funções */
+    void **f = (void **)malloc(k * sizeof(void *));
+    for (int i = 0; i < k; i++) {
+        scanf("%s", funcao);
+        f[i] = evaluator_create(funcao);
+    }
+
+    /* Lê epsilon e número máximo de iterações */
+    scanf("%lf %d", &epsilon, &maxIt);
+
+    /* Criar matriz */
+
+    /* Resolver por Gauss-Seidel */
+
+    /* Imprime tempo de execução */
+    printf("%lf\n", timestamp());
+
     return 0;
 }
--- makefile
+++ makefile
@@ -2,15 +2,18 @@
 
 CFLAGS = -Wall -lm -lmatheval
 
-objects = funcoes.o
+objects = funcoes.o utils.o
 
 all: ep02
 
-ep01: ep02.c $(objects)
+ep02: ep02.c $(objects)
 	gcc ep02.c -o ep02 $(objects) $(CFLAGS)
 
 funcoes.o: funcoes.c funcoes.h
 	gcc -c funcoes.c $(CFLAGS)
+
+utils.o: utils.c utils.h
+	gcc -c utils.c
 
 debug: CFLAGS += -g 
 debug: all
